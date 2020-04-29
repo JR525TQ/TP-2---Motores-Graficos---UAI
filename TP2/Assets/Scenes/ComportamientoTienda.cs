6083f5e4f304f40860e70fb2535caca7c550fda8
Belén <belen.daiana.h@gmail.com>
2020-04-29 05:26:55 -0300

    Fijate si te sirve :c

---
+++ TP2/Assets/Scenes/ComportamientoTienda.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class ComportamientoTienda : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    public class ItemTienda
+    {
+        public string Nombre { get; set; }
+
+        public int Costo { get; set; }
+    }
+
+    ItemTienda<string, int> itemtienda = new ItemTienda<string, int>();
+
+
+    
+
+
+    void Update()
+    {
+        
+    }
+}
